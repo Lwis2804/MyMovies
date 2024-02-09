7490496c1fdcf2e308f0ac1a745c425b50450c18
LUIS <luis.enrique.enc@gmail.com>
2024-02-08 19:23:39 -0600

    Initial Commit

---
+++ MyMovies/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyMovies
+//
+//  Created by LUIS GONZALEZ on 08/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
